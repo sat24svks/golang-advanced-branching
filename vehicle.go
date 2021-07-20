411663f27fd4ea07abec0f73c4ec93cb8bdad49c
sat24svks <sat24svks@gmail.com>
2021-07-20 22:43:59 +0530

    Update - Interface Variables define
    Interface Variables define

--- vehicle.go
+++ vehicle.go
@@ -1,13 +1,19 @@
 package main
 
-import (
-
-)
-
-
 // Values array for the feedback.json file
+type Values struct {
+	Models []Model `json: "values"`
+}
 
 // Model array for the feedback.json file
+type Model struct {
+	Name     string   `json: "model"`
+	Feedback []string `json: "feedback"`
+}
+
+type feedbackResult struct {
+	feedbackTotal, feedbackPositive, feedbackNegative, feedbackNeutral int
+}
 
 type rating float32
 
@@ -19,9 +25,28 @@ const (
 	extraNegative rating = -1.2
 )
 
+type vehicle interface {
+}
+
+type car struct {
+	model, make, typeVehicle string
+}
+
+type truck struct {
+	model, make, typeVehicle string
+}
+
+type bike struct {
+	model, make string
+}
+
+var (
+	vehicleResult map[string]feedbackResult
+	inventory     []vehicle
+)
+
 func init() {
 
-/*
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -34,15 +59,15 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+
+	//	vehicleResult = make(map[string]feedbackResult)
 
 }
 
 func main() {
 
 	// Generate ratings for the different vehicles
-	
+
 	// Print ratings for the different vehicles
 }
 
